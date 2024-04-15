ae138d365785fff710fd86b1f99bfce9d6726b4e
snwtr7 <spam@wehkamp.de>
2024-04-15 10:43:51 +0200

    update config, imgs

--- frontend/config.php
+++ frontend/config.php
@@ -4,19 +4,19 @@
 // GENERAL
 
 // Page title for your site
-define('title', 'Sleeky theme for YOURLS'); 
+define('title', 'JCNetwork URL Shortener'); 
 
 // The short title of your site, used in the footer and in some sub pages
-define('shortTitle', 'Sleeky');
+define('shortTitle', 'JCNetwork');
 
 // A description of your site, shown on the homepage.
-define('description', 'A quick description on why your site is so fantastic, what it does and why people should definitely start using it. Oh, and how it’s free.'); 
+define('description', 'JCNET.WORK ist der Link Kürzer des JCNetwork e.V. Er ermöglicht das Verkürzen von Links, das nachträgliche Bearbeiten, sowie das Generieren von Statistiken und QR-Codes. Die Verwendung ist ausschließlich für verbandsnahe Zwecke erlaubt. Keine Haftung für externe Inhalte.'); 
 
 // The favicon for your site
 define('favicon', '/frontend/assets/img/favicon.ico');
 
 // Logo for your site, displayed on home page
-define('logo', '/frontend/assets/img/logo-black.png');
+define('logo', '/frontend/assets/img/logo-portraits.jpg');
 
 // Enable reCAPTCHA V3
 // It is highly recommended you use reCAPTCHA V3. It will stop spam. You can get a site and secret key from here: https://www.google.com/recaptcha/admin/create
@@ -36,7 +36,7 @@ define('enableCustomURL', true);
 // Set a primary colour to be used. Default: #007bff
 // Here are some other colours you could try:
 // #f44336: red, #9c27b0: purple, #00bcd4: teal, #ff5722: orange
-define('colour', '#007bff');
+define('colour', '#224466');
 
 // Optional
 // Set a background image to be used.
@@ -50,9 +50,9 @@ define('colour', '#007bff');
 // The array follows a title link structure:
 // "TITLE" => "LINK",
 $footerLinks = [
-    "About"   =>  "https://sleeky.flynntes.com/",
-    "Contact" =>  "https://yourls.org/",
-    "Legal"   =>  "https://yourls.org/",
+    "Website" =>  "https://jcnetwork.de/",
+    "Impressum" =>  "https://jcnetwork.de/impressum/",
+    "Datenschutz"   =>  "https://jcnetwork.de/datenschutzerklaerung/",
     "Admin"   =>  "/admin"
 ];
 
